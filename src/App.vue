a45a51b540392009579eed009bae602c467b0625
Lypzis <victorvp2018@protonmail.com>
2021-03-30 21:43:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<section>
+		<header>
+			<h1>Users</h1>
+		</header>
+
+		<div>
+			<active-user :username="username" :age="age"></active-user>
+			<user-data
+				@update-name="updateName"
+				@update-age="updateAge"
+			></user-data>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username: '',
+				age: '',
+			};
+		},
+		methods: {
+			updateName(text) {
+				this.username = text;
+			},
+			updateAge(number) {
+				this.age = number;
+			},
+		},
+	};
+</script>
---
+++ src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<input
+			type="text"
+			placeholder="type the name"
+			v-model="username"
+			@input="changeUsername"
+		/>
+		<input
+			type="number"
+			placeholder="type the age"
+			v-model="age"
+			@input="changeAge"
+		/>
+	</div>
+</template>
+
+<script>
+	export default {
+		emits: ['update-name', 'update-age'],
+		data() {
+			return {
+				username: '',
+				age: '',
+			};
+		},
+		methods: {
+			changeUsername(event) {
+				this.$emit('update-name', event.target.value);
+			},
+			changeAge(event) {
+				this.$emit('update-age', event.target.value);
+			},
+		},
+	};
+</script>
